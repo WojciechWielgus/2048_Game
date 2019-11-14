e028e25ad64be435346730be91fe846591eeb736
WojciechWielgus <WojciechWielgus@users.noreply.github.com>
2019-11-14 15:47:43 +0100

    Start, movement

---
+++ Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace _2048
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.field33 = new System.Windows.Forms.Label();
+            this.field32 = new System.Windows.Forms.Label();
+            this.field31 = new System.Windows.Forms.Label();
+            this.field30 = new System.Windows.Forms.Label();
+            this.field23 = new System.Windows.Forms.Label();
+            this.field22 = new System.Windows.Forms.Label();
+            this.field21 = new System.Windows.Forms.Label();
+            this.field20 = new System.Windows.Forms.Label();
+            this.field13 = new System.Windows.Forms.Label();
+            this.field12 = new System.Windows.Forms.Label();
+            this.field11 = new System.Windows.Forms.Label();
+            this.field10 = new System.Windows.Forms.Label();
+            this.field03 = new System.Windows.Forms.Label();
+            this.field02 = new System.Windows.Forms.Label();
+            this.field01 = new System.Windows.Forms.Label();
+            this.field00 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.field33);
+            this.panel1.Controls.Add(this.field32);
+            this.panel1.Controls.Add(this.field31);
+            this.panel1.Controls.Add(this.field30);
+            this.panel1.Controls.Add(this.field23);
+            this.panel1.Controls.Add(this.field22);
+            this.panel1.Controls.Add(this.field21);
+            this.panel1.Controls.Add(this.field20);
+            this.panel1.Controls.Add(this.field13);
+            this.panel1.Controls.Add(this.field12);
+            this.panel1.Controls.Add(this.field11);
+            this.panel1.Controls.Add(this.field10);
+            this.panel1.Controls.Add(this.field03);
+            this.panel1.Controls.Add(this.field02);
+            this.panel1.Controls.Add(this.field01);
+            this.panel1.Controls.Add(this.field00);
+            this.panel1.Location = new System.Drawing.Point(284, 117);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(217, 218);
+            this.panel1.TabIndex = 0;
+            // 
+            // field33
+            // 
+            this.field33.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.field33.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.field33.Location = new System.Drawing.Point(162, 162);
+            this.field33.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.field33.Name = "field33";
+            this.field33.Size = new System.Drawing.Size(50, 50);
+            this.field33.TabIndex = 15;
+            this.field33.Text = "2";
+            this.field33.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // field32
+            // 
+            this.field32.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.field32.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.field32.Location = new System.Drawing.Point(109, 162);
+            this.field32.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.field32.Name = "field32";
+            this.field32.Size = new System.Drawing.Size(50, 50);
+            this.field32.TabIndex = 14;
+            this.field32.Text = "2";
+            this.field32.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // field31
+            // 
+            this.field31.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.field31.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.field31.Location = new System.Drawing.Point(56, 162);
+            this.field31.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.field31.Name = "field31";
+            this.field31.Size = new System.Drawing.Size(50, 50);
+            this.field31.TabIndex = 13;
+            this.field31.Text = "2";
+            this.field31.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // field30
+            // 
+            this.field30.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.field30.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.field30.Location = new System.Drawing.Point(3, 162);
+            this.field30.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.field30.Name = "field30";
+            this.field30.Size = new System.Drawing.Size(50, 50);
+            this.field30.TabIndex = 12;
+            this.field30.Text = "2";
+            this.field30.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // field23
+            // 
+            this.field23.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.field23.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.field23.Location = new System.Drawing.Point(162, 109);
+            this.field23.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.field23.Name = "field23";
+            this.field23.Size = new System.Drawing.Size(50, 50);
+            this.field23.TabIndex = 11;
+            this.field23.Text = "2";
+            this.field23.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // field22
+            // 
+            this.field22.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.field22.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.field22.Location = new System.Drawing.Point(109, 109);
+            this.field22.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.field22.Name = "field22";
+            this.field22.Size = new System.Drawing.Size(50, 50);
+            this.field22.TabIndex = 10;
+            this.field22.Text = "2";
+            this.field22.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // field21
+            // 
+            this.field21.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.field21.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.field21.Location = new System.Drawing.Point(56, 109);
+            this.field21.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.field21.Name = "field21";
+            this.field21.Size = new System.Drawing.Size(50, 50);
+            this.field21.TabIndex = 9;
+            this.field21.Text = "2";
+            this.field21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // field20
+            // 
+            this.field20.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.field20.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.field20.Location = new System.Drawing.Point(3, 109);
+            this.field20.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.field20.Name = "field20";
+            this.field20.Size = new System.Drawing.Size(50, 50);
+            this.field20.TabIndex = 8;
+            this.field20.Text = "2";
+            this.field20.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // field13
+            // 
+            this.field13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.field13.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.field13.Location = new System.Drawing.Point(162, 56);
+            this.field13.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.field13.Name = "field13";
+            this.field13.Size = new System.Drawing.Size(50, 50);
+            this.field13.TabIndex = 7;
+            this.field13.Text = "2";
+            this.field13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // field12
+            // 
+            this.field12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.field12.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.field12.Location = new System.Drawing.Point(109, 56);
+            this.field12.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.field12.Name = "field12";
+            this.field12.Size = new System.Drawing.Size(50, 50);
+            this.field12.TabIndex = 6;
+            this.field12.Text = "2";
+            this.field12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // field11
+            // 
+            this.field11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.field11.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.field11.Location = new System.Drawing.Point(56, 56);
+            this.field11.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.field11.Name = "field11";
+            this.field11.Size = new System.Drawing.Size(50, 50);
+            this.field11.TabIndex = 5;
+            this.field11.Text = "2";
+            this.field11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // field10
+            // 
+            this.field10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.field10.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.field10.Location = new System.Drawing.Point(3, 56);
+            this.field10.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.field10.Name = "field10";
+            this.field10.Size = new System.Drawing.Size(50, 50);
+            this.field10.TabIndex = 4;
+            this.field10.Text = "2";
+            this.field10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // field03
+            // 
+            this.field03.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.field03.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.field03.Location = new System.Drawing.Point(162, 3);
+            this.field03.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.field03.Name = "field03";
+            this.field03.Size = new System.Drawing.Size(50, 50);
+            this.field03.TabIndex = 3;
+            this.field03.Text = "2";
+            this.field03.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // field02
+            // 
+            this.field02.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.field02.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.field02.Location = new System.Drawing.Point(109, 3);
+            this.field02.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.field02.Name = "field02";
+            this.field02.Size = new System.Drawing.Size(50, 50);
+            this.field02.TabIndex = 2;
+            this.field02.Text = "2";
+            this.field02.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // field01
+            // 
+            this.field01.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.field01.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.field01.Location = new System.Drawing.Point(56, 3);
+            this.field01.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.field01.Name = "field01";
+            this.field01.Size = new System.Drawing.Size(50, 50);
+            this.field01.TabIndex = 1;
+            this.field01.Text = "2";
+            this.field01.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // field00
+            // 
+            this.field00.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.field00.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.field00.Location = new System.Drawing.Point(3, 3);
+            this.field00.Margin = new System.Windows.Forms.Padding(3, 3, 0, 0);
+            this.field00.Name = "field00";
+            this.field00.Size = new System.Drawing.Size(50, 50);
+            this.field00.TabIndex = 0;
+            this.field00.Text = "2";
+            this.field00.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "2048";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label field00;
+        private System.Windows.Forms.Label field33;
+        private System.Windows.Forms.Label field32;
+        private System.Windows.Forms.Label field31;
+        private System.Windows.Forms.Label field30;
+        private System.Windows.Forms.Label field23;
+        private System.Windows.Forms.Label field22;
+        private System.Windows.Forms.Label field21;
+        private System.Windows.Forms.Label field20;
+        private System.Windows.Forms.Label field13;
+        private System.Windows.Forms.Label field12;
+        private System.Windows.Forms.Label field11;
+        private System.Windows.Forms.Label field10;
+        private System.Windows.Forms.Label field03;
+        private System.Windows.Forms.Label field02;
+        private System.Windows.Forms.Label field01;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2048
+{
+    public partial class Form1 : Form
+    {
+        static int NUMBER_OF_START_NUMBERS = 7;
+        const int sizeBoard = 4;
+        List<Cell> cells = new List<Cell>();
+        enum Direction { Left, Up, Right, Down };
+
+        public Form1()
+        {
+            InitializeComponent();
+            Start();
+            UpdateBoard();
+            
+        }
+
+        void Start()
+        {
+            for (int i = 0; i < sizeBoard; i++)
+            {
+                for (int j = 0; j < sizeBoard; j++)
+                {
+                    string labelName = "field" + i + j;
+                    Label label = Controls.Find(labelName, true).FirstOrDefault() as Label;
+                    cells.Add(new Cell(i, j, 0, label));
+                }
+            }
+            StartRandomValues();
+        }
+
+        void Movement(Direction direction)
+        {
+            List<int> tempList = new List<int>();
+            for (int i = 0; i < sizeBoard; i++)
+            {
+                switch (direction)
+                {
+                    case Direction.Left:
+                        tempList.Clear();
+                        for (int j = 0; j < sizeBoard - 1; j++)
+                        {
+                            if (j < 1  && cells[(i * sizeBoard) + j].value == cells[(i * sizeBoard) + j + 3].value)
+                            {
+                                cells[(i * sizeBoard) + j].value *= 2;
+                                cells[(i * sizeBoard) + j + 3].value = 0;
+                            }
+                            else if (j < 2 && cells[(i * sizeBoard) + j].value == cells[(i * sizeBoard) + j + 2].value)
+                            {
+                                cells[(i * sizeBoard) + j].value *= 2;
+                                cells[(i * sizeBoard) + j + 2].value = 0;
+                            }
+                            else if (cells[(i * sizeBoard) + j].value == cells[(i * sizeBoard) + j + 1].value)
+                            {
+                                cells[(i * sizeBoard) + j].value *= 2;
+                                cells[(i * sizeBoard) + j + 1].value = 0;
+                            }
+                        }
+                        for (int j = 0; j < sizeBoard; j++)
+                        {
+                            if (cells[(i * sizeBoard) + j].value > 0)
+                            {
+                                tempList.Add(cells[(i * sizeBoard) + j].value);
+                                cells[(i * sizeBoard) + j].value = 0;
+                            }                            
+                        }
+                        for (int j = 0; j < tempList.Count; j++)
+                        {
+                            cells[(i * sizeBoard) + j].value = tempList[j];
+                        }
+
+
+                        
+                        break;
+                    case Direction.Up:
+                        tempList.Clear();
+                        for (int j = 0; j < sizeBoard; j++)
+                        {
+                            if (cells[i + (j * sizeBoard)].value > 0)
+                            {
+                                tempList.Add(cells[i + (j * sizeBoard)].value);
+                                cells[i + (j * sizeBoard)].value = 0;
+                            }
+                        }
+                        for (int j = 0; j < tempList.Count; j++)
+                        {
+                            cells[i + (j * sizeBoard)].value = tempList[j];
+                        }
+
+                        break;
+                    case Direction.Right:
+                        tempList.Clear();
+                        for (int j = 0; j < sizeBoard - 1; j++)
+                        {
+                            if(j < 1 && cells[(i * sizeBoard) + sizeBoard - 1 - j].value == cells[(i * sizeBoard) + sizeBoard - 4 - j].value)
+                            {
+                                cells[(i * sizeBoard) + sizeBoard - 1 - j].value *= 2;
+                                cells[(i * sizeBoard) + sizeBoard - 4 - j].value = 0;
+                            }
+                            else if (j < 2 && cells[(i * sizeBoard) + sizeBoard - 1 - j].value == cells[(i * sizeBoard) + sizeBoard - 3 - j].value)
+                            {
+                                cells[(i * sizeBoard) + sizeBoard - 1 - j].value *= 2;
+                                cells[(i * sizeBoard) + sizeBoard - 3 - j].value = 0;
+                            }
+                            else if (cells[(i * sizeBoard) + sizeBoard - 1 - j].value == cells[(i * sizeBoard) + sizeBoard - 2 - j].value)
+                            {
+                                cells[(i * sizeBoard) + sizeBoard - 1 - j].value *= 2;
+                                cells[(i * sizeBoard) + sizeBoard - 2 - j].value = 0;
+                            }
+                        }
+                        for (int j = 0; j < sizeBoard - 1; j++)
+                        {
+                            if (cells[(i * sizeBoard) + sizeBoard - 1 - j].value == cells[(i * sizeBoard) + sizeBoard - 2 - j].value)
+                            {
+                                cells[(i * sizeBoard) + sizeBoard - 1 - j].value *= 2;
+                                cells[(i * sizeBoard) + sizeBoard - 2 - j].value = 0;
+                            }
+                        }
+                        for (int j = 0; j < sizeBoard; j++)
+                        {
+                            if (cells[(i * sizeBoard) + j].value > 0)
+                            {
+                                tempList.Add(cells[(i * sizeBoard) + j].value);
+                                cells[(i * sizeBoard) + j].value = 0;
+                            }
+                        }
+                        for (int j = 0; j < tempList.Count; j++)
+                        {
+                            cells[(i * sizeBoard) + (sizeBoard - 1 - j)].value = tempList[tempList.Count - 1 - j];
+                        }
+                        break;
+                    case Direction.Down:
+                        tempList.Clear();
+                        for (int j = 0; j < sizeBoard; j++)
+                        {
+                            if (cells[i + (j * sizeBoard)].value > 0)
+                            {
+                                tempList.Add(cells[i + (j * sizeBoard)].value);
+                                cells[i + (j * sizeBoard)].value = 0;
+                            }
+                        }
+                        for (int j = 0; j < tempList.Count; j++)
+                        {
+                            cells[((sizeBoard * sizeBoard) - sizeBoard + i) - 4*j].value = tempList[tempList.Count - 1 - j];
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+        }
+
+        void StartRandomValues()
+        {
+            Random random = new Random();
+            Random random2 = new Random();
+            for (int i = 0; i < NUMBER_OF_START_NUMBERS; i++)
+            {
+                int position = random.Next(0, cells.Count);
+                if (cells[position].value <= 0) cells[position].value = 2;
+                else i--;
+            }
+        }
+
+        void UpdateBoard()
+        {
+            foreach (var cell in cells)
+            {
+                Label label = cell.label;
+                if(cell.value <= 0) label.Text = "";
+                else label.Text = cell.value.ToString();
+                //Console.WriteLine("X:{0} || Y:{1} || Label:{2}", cell.x, cell.y, cell.label.Name);
+            }
+        }
+           
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) Movement(Direction.Left);
+            if (e.KeyCode == Keys.Up) Movement(Direction.Up);
+            if (e.KeyCode == Keys.Right) Movement(Direction.Right);
+            if (e.KeyCode == Keys.Down) Movement(Direction.Down);
+            //RandomInsert();
+            UpdateBoard();
+        }
+    }
+
+
+
+    class Cell
+    {
+        //uint id;
+        public int x { get; set; }
+        public int y { get; set; }
+        public int value { get; set; }
+        public Label label { get; set; }
+
+        public Cell(int x, int y, int value, Label label)
+        {
+            this.x = x;
+            this.y = y;
+            this.value = value;
+            this.label = label;
+        }
+
+        public void Swap(Cell cell)
+        {
+            Cell tempCell = this;
+
+            //this.x = cell.x;
+            //this.y = cell.y;
+            this.value = cell.value;
+            //this.label = cell.label;
+
+            //cell.x = tempCell.x;
+            //cell.y = tempCell.y;
+            cell.value = tempCell.value;
+            //cell.label = tempCell.label;
+        }
+
+    }
+}
